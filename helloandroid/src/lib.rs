8fa9282c737ac112e3cb4df887979730541585de
Digvijay Upadhyay <digvijay.upadhyay@roxorgaming.com>
2022-01-09 12:55:20 +0000

    added android project

---
+++ helloandroid/src/lib.rs
@@ -0,0 +1,32 @@
+extern crate jni;
+
+use std::os::raw::{c_char};
+use std::ffi::{CString};
+
+use jni::JNIEnv;
+use jni::objects::{JClass, JObject, JValue};
+
+use hello::hello;
+
+pub type Callback = unsafe extern "C" fn(*const c_char) -> ();
+
+#[no_mangle]
+#[allow(non_snake_case)]
+pub extern "C" fn invokeCallbackViaJNA(callback: Callback) {
+    let s = CString::new(hello::greetings_from_rust()).unwrap();
+    unsafe { callback(s.as_ptr()); }
+}
+
+#[no_mangle]
+#[allow(non_snake_case)]
+pub extern "C" fn Java_com_roxorgaming_rustspike_android_MainActivity_invokeCallbackViaJNI(
+    env: JNIEnv,
+    _class: JClass,
+    callback: JObject
+) {
+    let s = String::from(hello::greetings_from_rust());
+    let response = env.new_string(&s)
+        .expect("Couldn't create java string!");
+    env.call_method(callback, "callback", "(Ljava/lang/String;)V",
+                    &[JValue::from(JObject::from(response))]).unwrap();
+}
